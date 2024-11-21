38249f893d2fbbcd29dbc8767fa0c06878c90056
Wahyu <firmansyahw503@gmail.com>
2024-11-21 10:15:04 +0700

    Commit 3 (mendefinisikan rute navigasi : Form)

--- app/src/main/java/com/example/praktikum_pertemuan7/ui/viewmodel/NavigationControl.kt
+++ app/src/main/java/com/example/praktikum_pertemuan7/ui/viewmodel/NavigationControl.kt
@@ -1,5 +1,7 @@
 package com.example.praktikum_pertemuan7.ui.viewmodel
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -15,39 +17,5 @@ import com.example.praktikum_pertemuan7.ui.view.DetailMahasiswaView
 import com.example.praktikum_pertemuan7.ui.view.FormMahasiswaView
 
 enum class Halaman {
-    DETAILMAHASISWA,
-    FORMMAHASISWA
-}
-
-@Composable
-fun NavigatorControl(
-    viewModel: MahasiswaViewModel = viewModel(),
-    navHost: NavHostController = rememberNavController(),
-    modifier: Any
-) {
-    val uiState by viewModel.dataModel.collectAsState()
-    NavHost(
-        navController = navHost,
-        startDestination = Halaman.FORMMAHASISWA.name
-    ) {
-        composable(route = Halaman.FORMMAHASISWA.name) {
-            val konteks = LocalContext.current
-            FormMahasiswaView(
-                listGender = ListGender.listGender.map { id -> konteks.getString(id) },
-                onSubmitClick = { listData ->
-                    viewModel.saveDataMhs(listData)
-                    navHost.navigate(Halaman.DETAILMAHASISWA.name)
-                }
-            )
-        }
-
-        composable(route = Halaman.DETAILMAHASISWA.name) {
-            DetailMahasiswaView(
-                dataMhs = uiState,
-                modifier = Modifier,
-                onBackButton = { navHost.popBackStack() }
-            )
-        }
-
-    }
+    Form
 }
